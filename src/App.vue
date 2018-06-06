056de1d6c8d929d376ff17b773c91da885dded0e
Shohei Ohta <shohei.ot@gmail.com>
2018-06-06 22:51:11 +0900

    chore: modified App.vue code, delete src/index.js

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,14 @@
 <template>
-  <section class="section">
-    <div class="columns">
-      <div id="target" class="column">
-        <div class="parent">
-          <p>this is parent element</p>
-          <hr>
-          <div class="child">
-            <p>this is child element</p>
-            <hr>
-            <ul>
-              <li>the 1</li>
-              <li>the 2</li>
-              <li>the 3</li>
-              <li>the 4</li>
-              <li>the 5</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div class="column">
-        <HtmlCodeViewer el="#target"></HtmlCodeViewer>
+  <div class="columns">
+    <div id="target" class="column">
+      <div>
+        <span>YO</span>
       </div>
     </div>
-  </section>
+    <div class="column">
+      <HtmlCodeViewer el="#target"></HtmlCodeViewer>
+    </div>
+  </div>
 </template>
 
 <script>
