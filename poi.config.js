10b50278d9560c9b0a63173e4eb2806366774377
Shohei Ohta <shohei.ot@gmail.com>
2018-06-06 22:47:05 +0900

    chore: change poi entry file

--- poi.config.js
+++ poi.config.js
@@ -1,18 +1,8 @@
 const webpack = require('webpack')
 module.exports = {
-  entry: './src/install.js',
+  entry: './src/develop.js',
   babel: {
     jsx: 'vue'
   },
-  hash: false,
-  configureWebpack(config) {
-    if (typeof config.plugins === 'undefined') {
-      config.plugins = []
-    }
-    config.plugins.push(
-      new webpack.DefinePlugin({
-        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-      })
-    )
-  }
+  hash: false
 }
